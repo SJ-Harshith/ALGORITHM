a100c0596494f3851de53e867ae1ed53c379c942
Harshith S J <144507417+SJ-Harshith@users.noreply.github.com>
2024-05-16 23:42:11 -0700

    Create sort.c

---
+++ sort.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+int count=0;
+void sort(int a[],int n){
+	int i,j,pos,temp;
+	for(i=0;i<=n-2;i++){
+		pos=i;
+		for(j=i+1;j<=n-1;j++){
+			count++;
+			if(a[j]<a[pos])
+                  pos=j;
+		}
+		if(i!=pos){
+			temp=a[i];
+			a[i]=a[pos];
+			a[pos]=temp;
+		}
+	}
+}
+int main(){
+	int *a,n,i;
+	printf("enter n\n");
+	scanf("%d",&n);
+	a=(int*)malloc(n*sizeof(int));
+	for(i=0;i<n;i++){
+		a[i]=rand()%1001;
+		printf("%d\t",a[i]);
+	}
+	sort(a,n);
+	printf("\nsorted array elements\n");
+	for(i=0;i<n;i++)
+	printf("%d\n",a[i]);
+	printf("basic opp count=%d",count);
+}
