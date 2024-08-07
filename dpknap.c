11ff37f606e6a2d288ef2c856e7d9e9df0186434
Harshith S J <144507417+SJ-Harshith@users.noreply.github.com>
2024-08-07 19:16:06 +0530

    Update dpknap.c

--- dpknap.c
+++ dpknap.c
@@ -1,53 +1,48 @@
-#include <stdio.h>
-
-int max(int a, int b) {
-    return a > b ? a : b;
+#include<stdio.h>
+int max(int a,int b){
+    return a>b?a:b;
 }
-
-void DPknapsack(int n, int m, int w[], int p[]) {
-    int i, j, V[n + 1][m + 1];
-    for (i = 0; i <= n; i++) {
-        for (j = 0; j <= m; j++) {
-            if (i == 0 || j == 0) {
-                V[i][j] = 0;
-            } else if (j < w[i]) {
-                V[i][j] = V[i - 1][j];
-            } else {
-                V[i][j] = max(V[i - 1][j], V[i - 1][j - w[i]] + p[i]);
-            }
-            printf("%d\t", V[i][j]);
+void dp(int n,int m,int p[],int w[])
+{   int i,j,v[n+1][m+1];
+    for(i=0;i<=n;i++){
+        for(j=0;j<=m;j++)
+        {
+            if(i==0||j==0)
+                v[i][j]=0;
+            else if(j<w[i])
+                v[i][j]=v[i-1][j];
+            else
+                v[i][j]=max(v[i-1][j],v[i-1][j-w[i]]+p[i]);
+             printf("%d\t",v[i][j]);
         }
         printf("\n");
     }
-    printf("Optimal profit: %d\n", V[n][m]);
-    printf("Items selected: ");
-    int res = V[n][m];
-    j = m;
-    for (i = n; i > 0 && res > 0; i--) {
-        if (res == V[i - 1][j])
-            continue;
-        else {
-            printf("%d ", i);
-            res -= p[i];
-            j -= w[i];
-        }
-    }
     printf("\n");
+printf("optimal profit %d \n",v[n][m]);
+printf("item selected \n");
+while(n!=0){
+    if(v[n][m]!=v[n-1][m])
+        {
+            printf("%d\t",n);
+            m=m-w[n];
+        }
+    n--;
 }
-
-int main() {
-    int n, m, i;
-    printf("Enter the number of items: ");
-    scanf("%d", &n);
-    int w[n + 1], p[n + 1];
-    printf("Enter the weights of the items: ");
-    for (i = 1; i <= n; i++)
-        scanf("%d", &w[i]);
-    printf("Enter the profits of the items: ");
-    for (i = 1; i <= n; i++)
-        scanf("%d", &p[i]);
-    printf("Enter the capacity of the knapsack: ");
-    scanf("%d", &m);
-    DPknapsack(n, m, w, p);
-    return 0;
+}
+int main(){
+    int i,j,n,m;
+    printf("enter the n ");
+    scanf("%d",&n);
+    printf("enter the capacity ");
+    scanf("%d",&m);
+    printf("enter the weights\n");
+    int p[10],w[10];
+    for(i=1;i<=n;i++){
+        scanf("%d",&w[i]);
+    }
+    printf("enter the profits\n");
+    for(i=1;i<=n;i++){
+        scanf("%d",&p[i]);
+    }
+    dp(n,m,p,w);
 }
